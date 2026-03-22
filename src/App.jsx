daa2c8106c17b52b96bd556fe071a8acfd35d72b
Slim Abida <slimabida21@gmail.com>
2026-03-22 21:24:24 +0100

    Feat: creation des sections du portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
-
-
+import "./index.css"
+import { Navbar } from "./layout/Navbar"
+import { Hero } from "./section/Hero"
+import { About } from "./section/About"
+import { Experience } from "./section/Experience"
+import { Projects } from "./section/Projects"
+import { Contact } from "./section/Contact"
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
-    <>
-      
-    </>
+   <div className="min-h-screen overflow-x-hidden">
+      <Navbar />
+      <main>
+        <Hero/>
+        <About/>
+        <Experience/>
+        <Projects/>
+        <Contact/>
+      </main>
+   </div>
   )
 }
 
