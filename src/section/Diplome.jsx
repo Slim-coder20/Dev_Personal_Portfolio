e54b1e9e1246559ac7de18c8ad0fb72ae09d15dd
Slim Abida <slimabida21@gmail.com>
2026-03-23 21:51:25 +0100

    Refactor: suppression de la div Stats Badge du composant Hero.jsx

--- src/section/Diplome.jsx
+++ src/section/Diplome.jsx
@@ -1,5 +1,44 @@
 import React from 'react'
 
+const projects = [
+  {
+    title: "Amazone-clone",
+    descrioption:
+      "Application web e-commerce inspirée d’Amazon : authentification Firebase, panier, checkout et paiement en ligne via Stripe.",
+    image: "/projects/project1.png",
+    tags: ["React", "React Router 7", "Node.js", "Firebase", "Stripe"],
+    link: "https://e-clone-f1b94.web.app/",
+    github: "https://github.com/Slim-coder20/amazone-clone.git",
+  },
+  {
+    title: "Vite&Gouramnd",
+    descrioption:
+      "Application web full-stack permettant aux clients de commander des repas en ligne, avec des espaces dédiés pour les clients, employés et administrateurs.",
+    image: "/projects/project2.png",
+    tags: ["React", "CSS", "Node.js", "Supabase", "MongoDB", "Docker"],
+    link: "https://vite-gourmand.vercel.app/",
+    github: "https://github.com/Slim-coder20/vite-gourmand.git",
+  },
+  {
+    title: "FoodStore",
+    descrioption:
+      "Application e-commerce alimentaire full-stack avec une interface client (acheteurs) et une interface vendeur (gestion des produits et commandes).",
+    image: "/projects/project3.png",
+    tags: ["React", "React-hook-form","Tailwindcss", "Node.js","MongoDB","Stripe", "JWT", "Multer", "Cloudinary"],
+    link: "https://foodstorefront.vercel.app",
+    github: "https://github.com/Slim-coder20/greenCart-.git",
+  },
+  {
+    title: "Website Artist - Slim Abida",
+    descrioption:
+      "Site web officiel de Slim Abida avec système de e-commerce intégré pour la vente d'albums, système de traduction FR/EN, et gestion dynamique du contenu.",
+    image: "/projects/project4.png",
+    tags: ["Next.js16", "TypeScript","CSS Modules", "React Context API","Prisma","Postgres", "Supabase"],
+    link: "https://www.slimabida.fr/",
+    github: "https://github.com/Slim-coder20/my-web-site.git",
+  },
+];
+
 export const Diplome = () => {
   return (
     <section></section>
